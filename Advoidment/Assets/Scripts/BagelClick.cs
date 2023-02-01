3ad143227c328a966a1ffafcd308a226ef744505
EyeAmfour <fruitloopsgo@gmail.com>
2023-02-01 15:01:44 -0500

    Fixed clicking and set score to increment on bagel click

--- Advoidment/Assets/Scripts/BagelClick.cs
+++ Advoidment/Assets/Scripts/BagelClick.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class BagelClick : MonoBehaviour
-{
+
+public class BagelClick : MonoBehaviour {
     // Start is called before the first frame update
     //[SerializeField] GameObject begal;
+    public int Score { get { return score; } }
 
     private int score = 0;
+
+    public GameObject bagel;
+    public float bagelRadius;
+
     void Start()
     {
-        
+        bagelRadius = bagel.transform.localScale.x / 2.0f;
     }
 
     public void OnClick(InputValue value)
     {
-        score++;
+        Vector2 mousePos = Input.mousePosition;
+
+        if (Vector2.Distance(mousePos, bagel.transform.position) <= bagelRadius) {
+            score++;
+        }
+
     }
+
     // Update is called once per frame
     void Update()
     {
