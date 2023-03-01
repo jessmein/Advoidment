af213b2743c0eb902460ebc28970766a2b3435d3
AndyVece143 <89553507+AndyVece143@users.noreply.github.com>
2023-03-01 11:04:08 -0500

    Waiting Ad Began

---
+++ Advoidment/Assets/Scripts/Ads/Waiting/CloseButton.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CloseButton : MonoBehaviour
+{
+    public bool isClicked;
+    public Vector3 originalPosition;
+    public WaitingAd waitingAd;
+    private Vector3 scale;
+    SpriteRenderer sprender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scale = waitingAd.transform.localScale;
+        float[] pointsX = { -2 * scale.x, 2 * scale.x };
+        float[] pointsY = { -4 * scale.y, 4 * scale.y };
+
+        isClicked = false;
+        transform.localPosition = Vector3.zero;
+        originalPosition = transform.position;
+
+        transform.position = new Vector3(originalPosition.x + pointsX[Random.Range(0,2)], originalPosition.y + pointsY[Random.Range(0,2)], 0);
+        sprender = gameObject.GetComponent<SpriteRenderer>();
+        sprender.enabled = false;
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (sprender.enabled == true)
+        {
+            Debug.Log("Click");
+            isClicked = true;
+        }
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);
+        sprender.enabled = true;
+    }
+}
